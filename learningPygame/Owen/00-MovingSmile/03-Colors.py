2233ede30bbacabcc20dccb44bb6d24df676bd95
mobacdesign <fisherds+mobacdesign@gmail.com>
2019-07-08 16:59:16 -0400

    Smile

--- learningPygame/Owen/00-MovingSmile/03-Colors.py
+++ learningPygame/Owen/00-MovingSmile/03-Colors.py
@@ -1,10 +1,18 @@
-# TODO: Copy all of your   02-TheGameLoop.py   program and put it below this comment.
-# TODO    One way to do so is:
-# TODO      1. Inside  02-TheGameLoop.py,  do:
-# TODO           -- Control-A (to SELECT the entire contents of the file, then
-# TODO           -- Control-C (to COPY that entire selection)
-# TODO      2. Inside this file:
-# TODO           -- Click below this comment, then
-# TODO           -- Control-V (to PASTE the copied code into this file.
-
 # TODO: In this module we'll add color to the window
+
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill((255, 0, 255)) # red
+    screen.fill((0, 255, 0)) # green
+    screen.fill((255, 255, 0)) # yellow
+    screen.fill((51, 255, 255)) # cyan
+    screen.fill((153, 76, 0)) # brown
+    screen.fill((255, 0, 255)) # magenta
+    pygame.display.update()
--- learningPygame/Owen/00-MovingSmile/04-Drawing.py
+++ learningPygame/Owen/00-MovingSmile/04-Drawing.py
@@ -1,15 +1,39 @@
-# TODO: Copy all of your   03-Colors.py   program and put it below this comment.
-# TODO    One way to do so is:
-# TODO      1. Inside  03-Colors.py,  do:
-# TODO           -- Control-A (to SELECT the entire contents of the file, then
-# TODO           -- Control-C (to COPY that entire selection)
-# TODO      2. Inside this file:
-# TODO           -- Click below this comment, then
-# TODO           -- Control-V (to PASTE the copied code into this file.
-
-
 # TODO: In this module we'll start drawing a simple smiley face
 #  Yellow circle for the head
 #  Two black circle eyes
 #  Red rectangle (rect) mouth
 #  Red circle nose.
+
+
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((255, 0, 255)) # magenta
+
+    pygame.draw.circle(screen, (200,200,0), (320,240), 150)
+    pygame.draw.circle(screen, (255, 255, 255), (240, 160), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (400, 160), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (320, 240), 10)
+    pygame.draw.circle(screen, (255, 255, 255), (220, 300), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (240, 320), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (260, 340), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (288, 355), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (310, 362), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+    pygame.draw.circle(screen, (255, 255, 255), (270, 350), 20)
+
+    pygame.display.update()
