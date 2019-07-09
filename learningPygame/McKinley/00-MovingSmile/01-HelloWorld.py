97cc4e22f918065612fd2a9f5cd309b6fc717b3d
David Fisher <fisherds@rose-hulman.edu>
2019-07-09 08:37:17 -0400

    added the BouncingBall starting code

---
+++ learningPygame/McKinley/00-MovingSmile/01-HelloWorld.py
@@ -0,0 +1,27 @@
+# Authors: David Mutchler, Dave Fisher, and many others before them.
+
+print('Hello, World')
+print('hi there')
+print('one', 'two', 'through my shoe')
+print(3 + 9)
+print('3 + 9', 'versus', 3 + 9)
+
+# done:  After we talk together about the above, add PRINT statements that print:
+# done:    1. A Hello message to a friend.
+# done:    2. Two big numbers, followed by their sum.
+# done:    3. The result of 3,607 multiplied by 34,227.  (Hint: the result is interesting.)
+# done:    4. Anything else you like!
+
+print('hi mommy')
+print('2345 + 3467 =', 2345+3467)
+print('3607 * 34227 =', 3607*34227)
+
+
+for x in range(10):
+    print("I Love You Daddy!")
+
+for x in range(10):
+    print("I love you", x + 2)
+
+# for x in range(10):
+#     print(x)
