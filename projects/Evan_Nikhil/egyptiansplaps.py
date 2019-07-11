5be7e4160b8b5a43f1d3e91e2627b3860fb88ef6
evanm <evan.maxwell.slater@gmail.com>
2019-07-11 14:06:13 -0400

    added start to dealing out cards

--- projects/Evan_Nikhil/egyptiansplaps.py
+++ projects/Evan_Nikhil/egyptiansplaps.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 #-----------------------------------------------------------------------------Player
 class Player:
-    def __init__(self):
+    def __init__(self,deck):
         pass
 
     def place_card(self):
@@ -58,6 +58,19 @@ def main():
     pygame.display.set_caption("this is a test")
     screen = pygame.display.set_mode((2000, 1000))
     #---------------set up----------------------------------------------------------------------------set up
+    #setting up new deck and dealing out cards
+    new_deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A']
+    temp_deck = []
+    random.shuffle(new_deck)
+    print(len(new_deck))
+    print(new_deck)
+    for i in range(18):
+        temp_deck.append(new_deck[0])
+        new_deck.pop(0)
+    player1 = Player(temp_deck)
+    temp_deck = []
+    #player2
+    #player3
 
 
 
