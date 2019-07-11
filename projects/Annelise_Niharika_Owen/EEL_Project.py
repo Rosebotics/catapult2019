9a24a4cd79ce56bd8ab551e1137ca5c8ba3fc64a
mobacdesign <fisherds+mobacdesign@gmail.com>
2019-07-11 11:28:09 -0400

    finished space invaders

--- projects/Annelise_Niharika_Owen/EEL_Project.py
+++ projects/Annelise_Niharika_Owen/EEL_Project.py
@@ -3,6 +3,10 @@ import pygame, sys, random, time
 class WaterBottle:
 
 class Head:
+    def __init__(self, screen, x, y,):
+        self.screen = screen
+        self.x = x
+        self.y = y
 
 class Fish:
 
