b9fee49cbd5c1d802cbdbb2f87a06a712a562252
mobacdesign <fisherds+mobacdesign@gmail.com>
2019-07-11 11:55:12 -0400

    finished space invaders

--- projects/Annelise_Niharika_Owen/EEL_Project.py
+++ projects/Annelise_Niharika_Owen/EEL_Project.py
@@ -1,6 +1,8 @@
 import pygame, sys, random, time
 
 class WaterBottle:
+    pass
+
 
 class Head:
     def __init__(self, screen, x, y,):
@@ -11,17 +13,33 @@ class Head:
     def draw(self):
         pygame.draw.rect(self.screen, (255, 165, 0), (self.x, self.y), 5)
 
+    def move(self):
+        pass
+
 class Fish:
+    pass
 
 class Body:
+    pass
 
 class Scoreboard:
+    pass
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     pygame.display.set_caption("EEL!")
     screen = pygame.display.set_mode((650, 650))
+    level1_image = pygame.image.load('level_1.png')
+    screen.blit(level1_image, (0,0))
+    level1_image = pygame.transform.scale(level1_image, (IMAGE_SIZE, IMAGE_SIZE))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
     clock.tick(60)
+    pygame.display.update()\
+
 main()
