f6cc719c7c707a068d3576cd683b390d007e77dc
Isabella <pattersonigp@gmail.com>
2019-07-11 10:13:29 -0700

    algorymolto vicace

--- projects/Allison_Cassandra_Isabella/scratch.py
+++ projects/Allison_Cassandra_Isabella/scratch.py
@@ -6,6 +6,7 @@ class Dancer:
 
 class Blueright:
 
+
 class Pinkleft:
 
 
@@ -16,3 +17,10 @@ class Yellowup:
 
 
 class HPBar:
+    def __init__(self,screen):
+        self.screen = screen
+        self.score = 1000
+        self.font = pygame.font.Font(None, 30)
+    def draw(self):
+        text_as_image = self.font.render("Score:" + str(self.score), True, (230, 28, 203))
+        self.screen.blit(text_as_image, (5, 5))
