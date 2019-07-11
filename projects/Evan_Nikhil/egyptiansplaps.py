ac353f794c20e02003b93842ec11218ac9b57327
evanm <evan.maxwell.slater@gmail.com>
2019-07-11 15:00:15 -0400

    made card placment and button detection for slapping

--- projects/Evan_Nikhil/egyptiansplaps.py
+++ projects/Evan_Nikhil/egyptiansplaps.py
@@ -4,10 +4,17 @@ from pygame.locals import *
 #-----------------------------------------------------------------------------Player
 class Player:
     def __init__(self,deck):
-        pass
+        self.deck = deck
+        self.inout = 1
+        self.slap_time = 0
 
-    def place_card(self):
-        pass
+#add top card of deck to pot then remove card from deck
+    def place_card(self,pot):
+        pot.append(self.deck[0])
+        self.deck.pop(0)
+        #TODO remove the pront and prints
+        print(pot)
+        print(self.deck)
 
     def slap(self):
         pass
@@ -62,13 +69,14 @@ def main():
     new_deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A']
     temp_deck = []
     random.shuffle(new_deck)
-    print(len(new_deck))
-    print(new_deck)
+    #done remove prints
+   # print(len(new_deck))
+    #print(new_deck)
     for i in range(18):
         temp_deck.append(new_deck[0])
         new_deck.pop(0)
     player1 = Player(temp_deck)
-    print(temp_deck)
+  # print(temp_deck)
     temp_deck = []
 
     #player2
@@ -76,7 +84,7 @@ def main():
         temp_deck.append(new_deck[0])
         new_deck.pop(0)
     player2 = Player(temp_deck)
-    print(temp_deck)
+  #  print(temp_deck)
     temp_deck = []
 
     #player3
@@ -84,12 +92,15 @@ def main():
         temp_deck.append(new_deck[0])
         new_deck.pop(0)
     player3 = Player(temp_deck)
-    print(temp_deck)
+   # print(temp_deck)
     temp_deck = []
 
     #--------------------------------------------------------------------------------------------------------
     turn = 1
-
+    pot = []
+    centerpile = CenterPile()
+    detection = Detection()
+    challenge = Challenge()
 
 
 
@@ -109,17 +120,32 @@ def main():
             if event.type == QUIT:
                 sys.exit()
             #-----in for loop------------------------------------------------------------------------------in for event loop
+           #done remove prints
+            # this is button detection for card placment
             if pressed_keys[pygame.K_BACKQUOTE] and turn == 1:
-                print(new_deck)
+                #print(new_deck)
                 turn = 2
+                player1.place_card(pot)
 
             if pressed_keys[pygame.K_v] and turn == 2:
-                print(new_deck)
+               # print(new_deck)
                 turn = 3
+                player2.place_card(pot)
 
             if pressed_keys[pygame.K_o] and turn == 3:
-                print(new_deck)
+                #print(new_deck)
                 turn = 1
+                player3.place_card(pot)
+            #button presses for slapping
+            #todo removec Prints
+            if pressed_keys[pygame.K_1]:
+              #  print('1')
+
+            if pressed_keys[pygame.K_b]:
+             #   print('b')
+
+            if pressed_keys[pygame.K_p]:
+            #    print('p')
 
 
 
