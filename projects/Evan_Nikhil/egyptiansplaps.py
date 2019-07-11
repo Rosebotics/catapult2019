ce289fd33d0b6de61834bff2320fe3002d5f1017
evanm <evan.maxwell.slater@gmail.com>
2019-07-11 13:02:31 -0400

    set up skeleten code and the screen color now with classes and comments to orginize

--- projects/Evan_Nikhil/egyptiansplaps.py
+++ projects/Evan_Nikhil/egyptiansplaps.py
@@ -1,19 +1,63 @@
 import pygame, sys, random, time
 from pygame.locals import *
 
+#-----------------------------------------------------------------------------Player
+class Player:
+    def __init__(self):
+        pass
 
+    def place_card(self):
+        pass
 
+    def slap(self):
+        pass
 
+    def draw(self):
+        pass
 
+#---------------------------------------------------------------------------------------CenterPile
+class CenterPile:
+    def __init__(self):
+        pass
 
+    def draw(self):
+        pass
 
+#-----------------------------------------------------------------------------------Detection
+class Detection:
+    def __init__(self):
+        pass
+
+    def slap(self):
+        pass
+
+    def win(self):
+        pass
+
+    def new_round(self):
+        pass
+
+#-------------------------------------------------------------------------------Challenge
+class Challenge:
+    def __init__(self):
+        pass
+
+#detect to set up tries remaining
+    def card_detector(self):
+        pass
+
+#detect if card placed after challenge card is another challenge card
+    def battle(self):
+        pass
+
+#----------------------------------------------------------------------------------------
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     pygame.display.set_caption("this is a test")
     screen = pygame.display.set_mode((2000, 1000))
-    #---------------set up---------------------------------------------------------------------------------------------------------
+    #---------------set up----------------------------------------------------------------------------set up
 
 
 
@@ -22,7 +66,7 @@ def main():
 
 
 
-
+    #-----------------------------------------------------------------------------------------------------------
     while True:
         clock.tick(60)
 
@@ -30,12 +74,13 @@ def main():
             pressed_keys = pygame.key.get_pressed()
             if event.type == QUIT:
                 sys.exit()
-            #-----in for loop---------------------------------------------------------------------------------------------------
+            #-----in for loop------------------------------------------------------------------------------in for event loop
 
 
 
-        #------------out of for loop-------------------------------------------------------------------------------
+        #------------out of for loop-------------------------------------------------------------------out of for event loop
         screen.fill((220, 181, 121))
+        pressed_keys = pygame.key.get_pressed()
 
 
 
