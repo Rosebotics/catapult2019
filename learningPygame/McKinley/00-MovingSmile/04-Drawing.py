af8f558680dd09d4bae724a7d675b28bebc9a2e9
David Fisher <fisherds@rose-hulman.edu>
2019-07-17 11:06:11 -0400

    add trim images

--- learningPygame/McKinley/00-MovingSmile/04-Drawing.py
+++ learningPygame/McKinley/00-MovingSmile/04-Drawing.py
@@ -1,15 +1,29 @@
-# TODO: Copy all of your   03-Colors.py   program and put it below this comment.
-# TODO    One way to do so is:
-# TODO      1. Inside  03-Colors.py,  do:
-# TODO           -- Control-A (to SELECT the entire contents of the file, then
-# TODO           -- Control-C (to COPY that entire selection)
-# TODO      2. Inside this file:
-# TODO           -- Click below this comment, then
-# TODO           -- Control-V (to PASTE the copied code into this file.
-
-
 # TODO: In this module we'll start drawing a simple smiley face
 #  Yellow circle for the head
 #  Two black circle eyes
 #  Red rectangle (rect) mouth
 #  Red circle nose.
+
+import pygame
+import sys
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((0, 200, 200))
+
+
+    pygame.draw.circle(screen, (255,255,0), (300,300), 250)
+    pygame.draw.circle(screen, (0, 0, 0), (300, 300), 250, 5)
+
+    pygame.draw.circle(screen, (0, 0, 0), (225, 200), 20)
+    pygame.draw.circle(screen, (0, 0, 0), (375, 200), 20)
+
+    pygame.display.update()
